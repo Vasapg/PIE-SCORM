c22c68caef3ca5663f4e2700322b49d35a77cb60
Victor Sanz Ararat <victoralejandro.sanz@alumnos.upm.es>
2023-05-11 17:42:50 +0200

    fix url in config file

--- Self-Assesment-4/SCORM-PACKAGE/js/abrirDocumento.js
+++ Self-Assesment-4/SCORM-PACKAGE/js/abrirDocumento.js
@@ -3,76 +3,108 @@
 //Una vez elegido uno que no este hecho, se agrega a la variable de DoscHechos y se envía al servicio Moodle
 //De la respuesta del servicio Moodle, obtendremos el titulo, el texto y los errores establecidos.
 
-function abrirDocumento() {
-	
-	var array_de_documentos = localStorage.getItem("array_de_documentos");
-	array_de_documentos = JSON.parse(array_de_documentos);
-	
-	var docsHechos = localStorage.getItem("docsHechos");
-	docsHechos = JSON.parse(docsHechos);
-	var buleano = false;
-	while (!buleano) {
-		var aleatorio = Math.floor(Math.random() * (array_de_documentos.length - 0)) + 0;
-		if (!yaEstaHecho(docsHechos, array_de_documentos[aleatorio])) {
-			buleano = true;
+function abrirDocumento(URL){
+    fetch(URL)
+    .then(response => response.text())
+    .then(data => {
+		var xmlDoc = new DOMParser().parseFromString(data, "text/xml");
+		var titulo = xmlDoc.getElementsByTagName("title")[0].childNodes[0].nodeValue;
+		var texto = xmlDoc.getElementsByTagName("text")[0].childNodes[0].nodeValue;
+
+		var texto_errores = [];
+		var tipos_errores = [];
+		var pesos_errores = [];
+		var comments = [];
+		var numErrores = xmlDoc.getElementsByTagName("defect").length;
+
+		//Con este bucle buscamos guardar en los arrays los textos de error, con su tipo y pesos.
+		for (var i = 0; i < numErrores; i++) {
+			var txt_Error = xmlDoc.getElementsByTagName("defect")[i].childNodes[0].nodeValue;
+			var tipo_Error = xmlDoc.getElementsByTagName("defect")[i].getAttribute("type");
+			var peso_Error = xmlDoc.getElementsByTagName("defect")[i].getAttribute("weight");
+			var comment = xmlDoc.getElementsByTagName("defect")[i].getAttribute("comment");
+
+			texto_errores.push(txt_Error);
+			tipos_errores.push(tipo_Error);
+			pesos_errores.push(peso_Error);
+			comments.push(comment);
 		}
-	}
-	var xhttp = comprobarXHTTP();
-	docsHechos.push(array_de_documentos[aleatorio]);
-	//Aqui agregamos el docHecho a nuestra BBDD
-	localStorage.setItem("docsHechos", JSON.stringify(docsHechos));
-	xhttp.open("GET", "xml/" + array_de_documentos[aleatorio], false);
-	xhttp.send();
-	var xmlDoc = xhttp.responseXML;
-	var titulo = xmlDoc.getElementsByTagName("title")[0].childNodes[0].nodeValue;
-	var texto = xmlDoc.getElementsByTagName("text")[0].childNodes[0].nodeValue;
+		var arrayDeTitulos = localStorage.getItem("titulos");
+		arrayDeTitulos = JSON.parse(arrayDeTitulos);
+		arrayDeTitulos.push(titulo);
+		//Pusheamos toda la información necesario a la BBDD
+		localStorage.setItem("titulos", JSON.stringify(arrayDeTitulos));
+		localStorage.setItem("titulo", titulo);
+		localStorage.setItem("texto", formatearTexto(texto));
+		localStorage.setItem("textoSolucion", texto);
+		localStorage.setItem("texto_errores", JSON.stringify(texto_errores));
+		localStorage.setItem("tipos_errores", JSON.stringify(tipos_errores));
+		localStorage.setItem("pesos_errores", JSON.stringify(pesos_errores));
+		localStorage.setItem("comments", JSON.stringify(comments))
+		localStorage.setItem("global", xmlDoc.getElementsByTagName("Global")[0].childNodes[0].nodeValue)
 
-	var texto_errores = [];
-	var tipos_errores = [];
-	var pesos_errores = [];
-	var comments = [];
-	var numErrores = xmlDoc.getElementsByTagName("defect").length;
+		//Aqui preparamos el texto para presentarlo en formato HTML
+		var body = document.getElementById("documentoXML");
+		var tituloHTML = document.createElement("h1");
+		tituloHTML.textContent = titulo;
+		var textoHTML = document.createElement("p");
+		textoHTML.setAttribute("id", "doc");
+		textoHTML.innerHTML = formatearTexto(texto);
 
-	//Con este bucle buscamos guardar en los arrays los textos de error, con su tipo y pesos.
-	for (var i = 0; i < numErrores; i++) {
-		var txt_Error = xmlDoc.getElementsByTagName("defect")[i].childNodes[0].nodeValue;
-		var tipo_Error = xmlDoc.getElementsByTagName("defect")[i].getAttribute("type");
-		var peso_Error = xmlDoc.getElementsByTagName("defect")[i].getAttribute("weight");
-		var comment = xmlDoc.getElementsByTagName("defect")[i].getAttribute("comment");
-
-		texto_errores.push(txt_Error);
-		tipos_errores.push(tipo_Error);
-		pesos_errores.push(peso_Error);
-		comments.push(comment);
-	}
-	var arrayDeTitulos = localStorage.getItem("titulos");
-	arrayDeTitulos = JSON.parse(arrayDeTitulos);
-	arrayDeTitulos.push(titulo);
-	//Pusheamos toda la información necesario a la BBDD
-	localStorage.setItem("titulos", JSON.stringify(arrayDeTitulos));
-	localStorage.setItem("titulo", titulo);
-	localStorage.setItem("texto", formatearTexto(texto));
-	localStorage.setItem("textoSolucion", texto);
-	localStorage.setItem("texto_errores", JSON.stringify(texto_errores));
-	localStorage.setItem("tipos_errores", JSON.stringify(tipos_errores));
-	localStorage.setItem("pesos_errores", JSON.stringify(pesos_errores));
-	localStorage.setItem("comments", JSON.stringify(comments))
-	localStorage.setItem("global", xmlDoc.getElementsByTagName("Global")[0].childNodes[0].nodeValue)
-
-	//Aqui preparamos el texto para presentarlo en formato HTML
-	var body = document.getElementById("documentoXML");
-	var tituloHTML = document.createElement("h1");
-	tituloHTML.textContent = titulo;
-	var textoHTML = document.createElement("p");
-	textoHTML.setAttribute("id", "doc");
-	textoHTML.innerHTML = formatearTexto(texto);
-
-	var espacioHTML = document.createElement("hr");
-	body.appendChild(tituloHTML);
-	body.appendChild(espacioHTML);
-	body.appendChild(textoHTML);
-	body.appendChild(espacioHTML);
+		var espacioHTML = document.createElement("hr");
+		body.appendChild(tituloHTML);
+		body.appendChild(espacioHTML);
+		body.appendChild(textoHTML);
+		body.appendChild(espacioHTML);
+	})
+	.catch(error => console.error(error));
 }
+
+function getConfig() {
+    return fetch("https://raw.githubusercontent.com/Vasapg/PIE-SCORM/main/Self-Assesment-4/exercises/config.xml")
+      .then(response => response.text())
+      .then(data => {
+        var xmlDoc = new DOMParser().parseFromString(data, "text/xml");
+        localStorage.setItem("nEjercicio", 0);
+        localStorage.setItem("maxEjercicio", parseInt(xmlDoc.getElementsByTagName("numExercises")[0].childNodes[0].nodeValue));
+        var titulos = [];
+        var urls = [];
+        
+        // obtiene todas las etiquetas "title" y "url"
+        var titleTags = xmlDoc.getElementsByTagName("title");
+        var urlTags = xmlDoc.getElementsByTagName("url");
+        console.log(titleTags);
+        console.log(urlTags);
+        console.log(xmlDoc);
+        
+        // itera sobre las etiquetas y guarda los contenidos en los arrays
+        for (var i = 0; i < titleTags.length; i++) 
+        {
+          titulos.push(titleTags[i].textContent);
+          urls.push(urlTags[i].textContent);
+        }
+        console.log(titulos);
+        console.log(urls);
+        localStorage.setItem("urls", JSON.stringify(urls));
+        localStorage.setItem("titulos", JSON.stringify(titulos));
+      });
+  }
+  
+  async function getUrl() {
+    if (!localStorage.getItem("urls")) {
+      await getConfig();
+    }
+    var nEjercicio = parseInt(localStorage.getItem("nEjercicio"));
+    console.log(nEjercicio);
+
+    var url = JSON.parse(localStorage.getItem("urls"));
+    url = url[nEjercicio];
+
+    abrirDocumento(url);
+    nEjercicio = nEjercicio + 1;
+    localStorage.setItem("nEjercicio", nEjercicio);
+  }
+
 
 function comprobarXHTTP() {
 	var xhttp;
@@ -105,4 +137,4 @@ function formatearTexto(texto) {
 	return texto.replace(/[\n]/gi, "<br>");
 }
 
-window.onload = abrirDocumento();
+window.onload = getUrl();
